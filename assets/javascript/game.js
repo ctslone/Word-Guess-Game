00f01329cf9fe743f64c48ec00f45d40efeaaac5
ctslone <ctslone27@gmail.com>
2019-10-04 18:38:26 -0400

    adding more JS. added the word gets displayed as underscores and the guesses are displayed at the bottom

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 // page loads and all HTML elements and CSS is displayed
 // press any letter to begin guessing. document.on(click) will set off the start of the game
 // plage loads and newgame function runs and resets wins, guesses left, letters guessed
-// when page loads, a random word from the word array will be selected as the winning word but will only be displayed in dashes
-    // function will look at the random word selected and replace each letter in the word (mini array) with an underscore, separated by a space
+// when page loads, a random word from the word array will be selected as the winning word but will only be displayed in dashes --DONE--
+    // function will look at the random word selected and replace each letter in the word (mini array) with an underscore, separated by a space --DONE-- used a for loop
 // if the guessed letter does not match one of the letters in the random word:
     // subtract a guess and add the letter to the guessed letter array. 
 // IF the guessed letter does match a letter in the random word:
@@ -16,6 +16,7 @@
     // defining variables to be used during the game
     var wins = 0;
     var guessesLeft = 20;
+    var newCar = "";
     var lettersGuessed = [];
     var carNames = [
         {
@@ -49,19 +50,65 @@
         }
     ];
 
-
+    // function that runs when the game loads, the user wins or the user runs out of guesses
     function newGame() {
+        // declaring variables to be reset at the begininng of new game and used throughout the game
         guessesLeft = 20;
+        answerSpace = [];
         lettersGuessed = [];
         newCar = carNames[Math.floor(Math.random() * carNames.length)];
         console.log(newCar.name)
+        // looping through the array newcar.name and treating the car name as an array. for each index of the word, replace the letter (push) with an underscore
+        for (var i=0; i<newCar.name.length; i++) {
+                answerSpace.push ("_")
+                };
+        // writing to the various HTML elements to display wins, guesses left and blank spaces for the new word
+        document.getElementById("wins-text").innerHTML = "Wins: " + 0;
+        document.getElementById("guesses-left-text").innerHTML = "Guesses left: " + 20;
+        document.getElementById("letters-text").innerHTML = [];
+        document.getElementById("answer-space").innerHTML = answerSpace.join(" ");
+        };
 
-        winsText = document.getElementById("wins-text").innerHTML = "Wins: " + 0;
-        currentWordText = document.getElementById("current-word-text").innerHTML = "Current car to guess: " + newCar.name;
-        guessesText = document.getElementById("guesses-text").innerHTML = "Guesses left: " + 20;
-        lettersText = document.getElementById("letters-text").innerHTML = [];
-        
-        
-    }
+        // when user releases a letter key, run this function
+        document.onkeyup = function (event) {
+            // user key press is assigned to variable and made uppercase
+            var guessedLetter = event.key.toUpperCase();
+                console.log("The user guessed: " + guessedLetter);
+            // if user selects a letter they already guessed, dont do anything (ie return)
+
+            // if (guessedLetter.includes(lettersGuessed)) {
+            //     return;
+            // }
+
+            // adding guessed letters to the lettersGuessed array
+            if (event.keyCode > 64 && event.keyCode < 91) {
+                console.log ("User selected an eligible letter");
+                // assigning false to the userGuess by default unless the user guesses a correct letter
+                // var correctGuess = false;
+
+                // looping throught the winning word and checking to see if the user guessed letter matches any of the letters in the word. use charAt
+                for (var i=0; i < newCar.name.length; i++) {
+                    // if the guessedletter matches one of the letters in the random new car...
+                    if(guessedLetter === newCar.name.charAt(i)) {
+                        console.log("User guessed a correct letter");
+                        answerSpace[i] = guessedLetter;
+                        // correctGuess = true;  
+                    }
+                }
+                    // pushing the userguess into the guessed letter array and dispaying the guesses on the page
+                    lettersGuessed.push(guessedLetter + " ");
+                    document.getElementById("answer-space").innerHTML = answerSpace.join(" ");
+                    document.getElementById("letters-text").innerHTML = lettersGuessed;
+
+                if ()
+                    
+
+                    
+                    
+                
+                
+            }
+        };
+    
 newGame()
 
